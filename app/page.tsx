6daf0fefd83c3822825502fb6df508c793a4d2bf
Ahmed Assaf <ahmedassafofficial@gmail.com>
2026-09-22 09:06:37 +0300

    Final touch

--- app/page.tsx
+++ app/page.tsx
@@ -127,7 +127,7 @@ const PORTFOLIO_DATA = {
         ],
         tags: ["Advanced CBO", "Offer Testing", "Bundle Strategy", "Scaling"],
         resultsGallery: [
-          "/emirates-results.png"
+          "/emirates-perfumes.png"
         ]
       },
       {
